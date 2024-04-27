7655068bebb7b1069cc624cf33674e7c65b23d30
SuperManito <68613938+SuperManito@users.noreply.github.com>
2024-04-27 21:30:54 +0800

    fix: 修复由于重构生产环境部署目录而产生的错误

--- utils/notify.js
+++ utils/notify.js
@@ -1,5 +1,5 @@
 const title = process.argv[2]
 const content = process.argv[3]
 const useUserNotify = process.argv[4] === 'true'
-const notify = useUserNotify ? require(`${process.env.ARCADIA_DIR}/config/sendNotify`) : require(`${process.env.ARCADIA_DIR}/utils/sendNotify`)
+const notify = useUserNotify ? require(`${process.env.ARCADIA_DIR}/config/sendNotify`) : require('./sendNotify')
 notify.sendNotify(`${title}`, `${content}`)
