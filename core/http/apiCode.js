d0b095fa7061e2b5d6c2a600d8182529758515dd
SuperManito <68613938+SuperManito@users.noreply.github.com>
2024-05-03 02:32:18 +0800

    perf: 优化接口响应模板

--- core/http/apiCode.js
+++ core/http/apiCode.js
@@ -20,7 +20,7 @@ const API_STATUS_CODE = {
       type: 'error',
     }
   },
-  failData(message = '系统错误', result) {
+  failData(message = 'error', result) {
     return {
       message,
       result,
@@ -31,26 +31,30 @@ const API_STATUS_CODE = {
   API: {
     NEED_LOGIN: {
       code: 403,
-      message: '请先登录!',
+      message: '请先登录',
     },
     NO_PERMISSION: {
       code: 401,
-      message: '无权限访问!',
+      message: '无访问权限',
     },
     SYNTAX_ERROR: {
       code: 400,
-      message: '请求错误!',
+      message: '请求错误',
     },
   },
   OPEN_API: {
     AUTH_FAIL: {
       code: 4403,
-      type: '认证失败!',
+      type: '认证失败',
     },
     NOT_OPEN: {
       code: 4406,
       type: '调用失败',
-      message: '未设置开放接口密钥，不允许访问',
+      message: '请提供令牌',
+    },
+    SYNTAX_ERROR: {
+      code: 400,
+      message: '请求错误',
     },
   },
 }
