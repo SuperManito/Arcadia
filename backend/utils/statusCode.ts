f9ecafda695cb19da661712def0505aadfa26a93
Super Manito <68613938+SuperManito@users.noreply.github.com>
2026-09-11 16:52:35 +0800

    perf(backend): 将 API 错误消息翻转换为英文

--- backend/utils/statusCode.ts
+++ backend/utils/statusCode.ts
@@ -35,43 +35,16 @@ export default {
     }
   },
   API: {
-    NO_AUTH: {
-      code: 401,
-      message: '请先登录',
-    },
-    AUTH_FAIL: {
-      code: 403,
-      message: '认证失败',
-    },
-    SYNTAX_ERROR: {
-      code: 400,
-      message: '请求错误',
-    },
-    INTERNAL_ERROR: {
-      code: 500,
-      message: '服务器内部错误',
-    },
+    NO_AUTH: { code: 401, message: 'Please Log In First' },
+    AUTH_FAIL: { code: 403, message: 'Authentication Failed' },
+    SYNTAX_ERROR: { code: 400, message: 'Bad Request' },
+    INTERNAL_ERROR: { code: 500, message: 'Internal Server Error' },
   },
   OPEN_API: {
-    NO_AUTH: {
-      code: 4401,
-      message: '需要授权的应用程序接口（请提供令牌）',
-    },
-    AUTH_FAIL: {
-      code: 4403,
-      message: '认证失败',
-    },
-    PERMISSION_DENIED: {
-      code: 4405,
-      message: '权限不足',
-    },
-    NOT_FOUND: {
-      code: 4404,
-      message: '应用程序接口不存在，请查阅官方文档',
-    },
-    SYNTAX_ERROR: {
-      code: 4400,
-      message: '请求错误',
-    },
+    NO_AUTH: { code: 4401, message: 'API Authorization Required' },
+    AUTH_FAIL: { code: 4403, message: 'Authentication Failed' },
+    PERMISSION_DENIED: { code: 4405, message: 'Permission Denied' },
+    NOT_FOUND: { code: 4404, message: 'API Not Found' },
+    SYNTAX_ERROR: { code: 4400, message: 'Bad Request' },
   },
 }
